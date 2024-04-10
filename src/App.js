374f367f701c0ddacae96417b3ee82805678a5b3
Onas-2000 <onasanyadaniel794@gmail.com>
2024-04-10 17:47:59 +0100

    New Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import './App.css'
 import Accordion from "./components/accordion"
-import Backgroundpattern from './assets/styles/background-pattern-desktop';
-import Backgroundmobile from './assets/styles/background-pattern-mobile';
+
 
 
 
 function App(){
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(()=>{
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);
-        };
-        handleResize();
-        window.removeEventListener('resize', handleResize);
-    }, []);
+    
+   
     return(
-        <div>
-         {isMobile ? <Backgroundmobile/> : <Backgroundpattern />}
-         <Accordion/>
+        <div className='App'>
+          <div className='background-image'>
+
+          </div>
+         <Accordion />
         </div>
         
     )
